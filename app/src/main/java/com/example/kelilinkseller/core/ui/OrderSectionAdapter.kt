fbba4fe22f4eebee02701d4d8e6f343d1fd011a7
kartikasw <kartikasariwidagdo@gmail.com>
2022-11-08 07:31:54 +0700

    progress

---
+++ app/src/main/java/com/example/kelilinkseller/core/ui/OrderSectionAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.kelilinkseller.core.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.kelilinkseller.features.order.done.OrderDoneFragment
+import com.example.kelilinkseller.features.order.new_order.OrderNewFragment
+import com.example.kelilinkseller.features.order.ready.OrderReadyFragment
+
+class OrderSectionAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = OrderNewFragment()
+            1 -> fragment = OrderReadyFragment()
+            2 -> fragment = OrderDoneFragment()
+        }
+        return fragment as Fragment
+    }
+}
--- app/src/main/java/com/example/kelilinkseller/features/order/OrderFragment.kt
+++ app/src/main/java/com/example/kelilinkseller/features/order/OrderFragment.kt
@@ -5,18 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kelilinkseller.R
+import com.example.kelilinkseller.core.ui.OrderSectionAdapter
+import com.example.kelilinkseller.databinding.FragmentOrderBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OrderFragment : Fragment() {
 
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpToolbar()
+
+        setUpSections()
+    }
+
+    private fun setUpToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.oToolbar)
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            title = resources.getString(R.string.app_name)
+        }
+    }
+
+    private fun setUpSections() {
+        with(binding) {
+            val sectionAdapter = OrderSectionAdapter(requireActivity())
+            val viewPager = oViewpager
+            viewPager.adapter = sectionAdapter
+            TabLayoutMediator(oTab, viewPager) { tab, position ->
+                tab.text = resources.getString(TAB_TITLES[position])
+            }.attach()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_order_new,
+            R.string.tab_order_ready ,
+            R.string.tab_order_done
+        )
     }
 
 }
